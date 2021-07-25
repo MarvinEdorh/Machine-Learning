a498b4323965ec52de033542c94d66b994285cea
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-07-25 18:21:33 +0200

    Update future_transactions.sql

--- future_transactions.sql
+++ future_transactions.sql
@@ -1,13 +1,15 @@
 WITH visitors AS (
-SELECT fullvisitorid,device.deviceCategory,device.operatingSystem,trafficSource.campaign, trafficSource.medium, geoNetwork.continent,
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201701*` AS ga, UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp ), 
+SELECT fullvisitorid, device.deviceCategory, device.operatingSystem, 
+trafficSource.campaign, trafficSource.medium, geoNetwork.continent,
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201701*` AS ga, 
+UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp ), 
 visits_products AS (  
-SELECT fullvisitorid, hp.v2ProductName AS Products, SUM( totals.visits) AS Products_Visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201701*` AS ga,UNNEST(ga.hits) AS hits, 
-UNNEST(hits.product) AS hp 
+SELECT fullvisitorid, hp.v2ProductName AS Products, SUM(totals.visits) AS Products_Visits
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201701*` AS ga,
+UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
 GROUP BY fullvisitorid, Products ORDER BY fullvisitorid, Products_Visits DESC ), 
 visits_products_category AS (  
-SELECT fullvisitorid, hp.v2ProductCategory AS Products_Category, SUM( totals.visits) Products_Category_Visits
+SELECT fullvisitorid, hp.v2ProductCategory AS Products_Category, SUM(totals.visits) AS Products_Category_Visits
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201701*` AS ga,UNNEST(ga.hits) AS hits, 
 UNNEST(hits.product) AS hp 
 GROUP BY fullvisitorid, Products_Category ORDER BY fullvisitorid, Products_Category_Visits DESC )
