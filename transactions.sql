bf0ecc4a08f03d9bd0b26543636fe0eaaaed27f9
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-07-25 14:18:38 +0200

    Update transactions.sql

--- transactions.sql
+++ transactions.sql
@@ -3,19 +3,20 @@ transactions AS (
 SELECT hits.transaction.transactionId AS ID_Transaction, device.deviceCategory,device.operatingSystem,
 trafficSource.campaign, trafficSource.medium, geoNetwork.continent, fullvisitorid , hp.v2ProductName AS Products, 
 hp.v2ProductCategory AS Products_Category,IFNULL(SUM(hits.transaction.transactionRevenue/1000000),0) AS CA, 
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20161201*` AS ga, UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20161201*` AS ga, 
+UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
 GROUP BY ID_Transaction, device.deviceCategory,device.operatingSystem,trafficSource.campaign, 
 trafficSource.medium, geoNetwork.continent, fullvisitorid , Products, Products_Category
 ORDER BY CA DESC,ID_Transaction ), 
 visits_products AS (  
 SELECT fullvisitorid, hp.v2ProductName AS Products, SUM( totals.visits) AS Products_Visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp
-WHERE _TABLE_SUFFIX <= '20161201'
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, 
+UNNEST(hits.product) AS hp WHERE _TABLE_SUFFIX <= '20161201'
 GROUP BY fullvisitorid, Products ORDER BY fullvisitorid, Products_Visits DESC ), 
 visits_products_category AS (  
 SELECT fullvisitorid, hp.v2ProductCategory AS Products_Category, SUM( totals.visits) Products_Category_Visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp
-WHERE _TABLE_SUFFIX <= '20161201'
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, 
+UNNEST(hits.product) AS hp WHERE _TABLE_SUFFIX <= '20161201'
 GROUP BY fullvisitorid, Products_Category ORDER BY fullvisitorid, Products_Category_Visits DESC )
 SELECT ID_Transaction, deviceCategory, operatingSystem, campaign, medium, continent, transactions.fullvisitorid,
 transactions.Products, Products_Visits, transactions.Products_Category, Products_Category_Visits, CA FROM transactions
