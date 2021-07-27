1dded866af4fa43e4af02afb7ab121f8ca42d729
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-07-27 20:34:01 +0200

    Update transactions.sql

--- transactions.sql
+++ transactions.sql
@@ -1,28 +1,28 @@
 WITH 
 transactions AS (
 SELECT hits.transaction.transactionId AS ID_Transaction, device.deviceCategory,device.operatingSystem,
-trafficSource.campaign, trafficSource.medium, geoNetwork.continent, fullvisitorid , hp.v2ProductName AS Products, 
-hp.v2ProductCategory AS Products_Category,IFNULL(SUM(hits.transaction.transactionRevenue/1000000),0) AS CA, 
+trafficSource.campaign, trafficSource.medium, geoNetwork.continent, fullvisitorid , hp.v2ProductName AS Product, 
+hp.v2ProductCategory AS Product_Category,IFNULL(SUM(hits.transaction.transactionRevenue/1000000),0) AS CA, 
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20161201*` AS ga, 
 UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
 GROUP BY ID_Transaction, device.deviceCategory,device.operatingSystem,trafficSource.campaign, 
-trafficSource.medium, geoNetwork.continent, fullvisitorid , Products, Products_Category
+trafficSource.medium, geoNetwork.continent, fullvisitorid , Product, Product_Category
 ORDER BY CA DESC,ID_Transaction ), 
 visits_products AS (  
-SELECT fullvisitorid, hp.v2ProductName AS Products, SUM( totals.visits) AS Products_Visits
+SELECT fullvisitorid, hp.v2ProductName AS Product, SUM( totals.visits) AS Product_Visits
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, 
 UNNEST(hits.product) AS hp WHERE _TABLE_SUFFIX <= '20161201'
-GROUP BY fullvisitorid, Products ORDER BY fullvisitorid, Products_Visits DESC ), 
+GROUP BY fullvisitorid, Product ORDER BY fullvisitorid, Product_Visits DESC ), 
 visits_products_category AS (  
-SELECT fullvisitorid, hp.v2ProductCategory AS Products_Category, SUM( totals.visits) Products_Category_Visits
+SELECT fullvisitorid, hp.v2ProductCategory AS Product_Category, SUM( totals.visits) Product_Category_Visits
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga,UNNEST(ga.hits) AS hits, 
 UNNEST(hits.product) AS hp WHERE _TABLE_SUFFIX <= '20161201'
-GROUP BY fullvisitorid, Products_Category ORDER BY fullvisitorid, Products_Category_Visits DESC )
+GROUP BY fullvisitorid, Product_Category ORDER BY fullvisitorid, Product_Category_Visits DESC )
 SELECT ID_Transaction, deviceCategory, operatingSystem, campaign, medium, continent, transactions.fullvisitorid,
-transactions.Products, Products_Visits, transactions.Products_Category, Products_Category_Visits, CA FROM transactions
+transactions.Product, Product_Visits, transactions.Product_Category, Product_Category_Visits, CA FROM transactions
 LEFT JOIN visits_products
-ON transactions.fullvisitorid = visits_products.fullvisitorid AND transactions.Products = visits_products.Products
+ON transactions.fullvisitorid = visits_products.fullvisitorid AND transactions.Product = visits_products.Product
 LEFT JOIN visits_products_category
 ON transactions.fullvisitorid = visits_products_category.fullvisitorid 
-AND transactions.Products_Category = visits_products_category.Products_Category
+AND transactions.Product_Category = visits_products_category.Product_Category
 ORDER BY CA DESC
