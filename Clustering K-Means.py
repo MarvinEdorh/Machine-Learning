b2a45adbc8d3b3f5da2d9c83383769b66acadc73
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-07-29 13:06:48 +0200

    Update Clustering K-Means.py

--- Clustering K-Means.py
+++ Clustering K-Means.py
@@ -10,12 +10,12 @@ col = list(pays.columns); del col[0]
 
 pays_clusters = pd.DataFrame(np.c_[pays.iloc[:,1:4]], columns = col, index = pays['country']) 
 
-# Attention On appliquer le modele K-Means sur des variables numeriqes, 
+#On applique le modele K-Means sur des variables numeriqes, 
 #si varibles categorielles, les tranformer avec une analyse factorielle ou un label encoding en utlisant cat.codes 
 
 
 #On ne sait pas a priori quel est le nombre optimal de clusters pour que le population soit separer de maniere
-#à ce que les groupes constituées soit à la fois le plus homogenes possible et differents les un des autres
+#à ce que les groupes constituées soient à la fois le plus homogenes possible et differents les un des autres
 #on utlise pour cela la courbe d'elbow en testant une decomposition de 1 à 10 groupes
 from sklearn.cluster import KMeans ; import matplotlib.pyplot as plt
 
