f5ff2c952879dcbaf3e1765c0c2b364261efc537
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-08-16 21:45:50 +0200

    Update Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,13 +1,13 @@
 ############################################### Costumer Lifetime Value ##################################################
-import os; os.chdir('C:/Users/marvin/Python')
+import os; os.chdir('C:/Users/marvin/Desktop/Python')
 
 #################################################### SQL BigQuery ########################################################
 
 import numpy as np ; import pandas as pd ; from google.cloud import bigquery
 
 client = bigquery.Client.from_service_account_json(
-json_credentials_path='mrvtestproject45-bbc9aec8eae9.json', 
-project='mrvtestproject45')
+json_credentials_path='data_pipeline-bbc9aec8eae9.json', 
+project='data_pipeline')
 
 #CA, lifetime et lifetime value de chaque consommateur en 2016
 query = """
