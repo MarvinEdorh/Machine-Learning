9b8af01e63a2d4bf559cb1ff12a2283518fcdbf6
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-09-04 17:18:36 +0200

    Update transactions.sql

--- transactions.sql
+++ transactions.sql
@@ -10,37 +10,47 @@ UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp),
 
 product AS (
 SELECT fullvisitorid, hp.v2ProductName, 
-DATETIME(EXTRACT(YEAR FROM PARSE_DATE("%Y%m%d", date)), EXTRACT(MONTH FROM PARSE_DATE("%Y%m%d", date)),
-         EXTRACT(DAY FROM PARSE_DATE("%Y%m%d", date)), hits.hour, hits.minute, 00) AS datetime, SUM(totals.visits) AS visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
+       DATETIME(EXTRACT(YEAR FROM PARSE_DATE("%Y%m%d", date)), EXTRACT(MONTH FROM PARSE_DATE("%Y%m%d", date)),
+                EXTRACT(DAY FROM PARSE_DATE("%Y%m%d", date)), hits.hour, hits.minute, 00) AS datetime, 
+       SUM(totals.visits) AS visits
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, 
+UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
 WHERE _TABLE_SUFFIX <= '20161201' GROUP BY fullvisitorid, hp.v2ProductName, datetime),
 
 product_visits AS (
-SELECT fullvisitorid, v2ProductName, datetime, SUM(visits) 
-OVER(PARTITION BY fullvisitorid, v2ProductName ORDER BY datetime ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS product_visits FROM product),
+SELECT fullvisitorid, v2ProductName, datetime, 
+       SUM(visits) OVER(PARTITION BY fullvisitorid, v2ProductName ORDER BY datetime 
+                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS product_visits FROM product),
 
 category AS (
 SELECT fullvisitorid, hp.v2ProductCategory,
-DATETIME(EXTRACT(YEAR FROM PARSE_DATE("%Y%m%d", date)), EXTRACT(MONTH FROM PARSE_DATE("%Y%m%d", date)),
-         EXTRACT(DAY FROM PARSE_DATE("%Y%m%d", date)), hits.hour, hits.minute, 00) AS datetime, SUM(totals.visits) AS visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
+       DATETIME(EXTRACT(YEAR FROM PARSE_DATE("%Y%m%d", date)), EXTRACT(MONTH FROM PARSE_DATE("%Y%m%d", date)),
+                EXTRACT(DAY FROM PARSE_DATE("%Y%m%d", date)), hits.hour, hits.minute, 00) AS datetime, 
+       SUM(totals.visits) AS visits
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, 
+UNNEST(ga.hits) AS hits, UNNEST(hits.product) AS hp 
 WHERE _TABLE_SUFFIX <= '20161201' GROUP BY fullvisitorid, hp.v2ProductCategory, datetime),
 
-category_visits AS (SELECT fullvisitorid, v2ProductCategory, datetime,
-SUM(visits) OVER(PARTITION BY fullvisitorid, v2ProductCategory ORDER BY datetime ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS category_visits
-FROM category)
+category_visits AS (
+SELECT fullvisitorid, v2ProductCategory, datetime,
+       SUM(visits) OVER(PARTITION BY fullvisitorid, v2ProductCategory ORDER BY datetime 
+                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS category_visits FROM category)
 
 SELECT transactionId, deviceCategory, operatingSystem, campaign, medium, country, transactions.v2ProductCategory, 
 transactions.v2ProductName, price, product_visits.product_visits, category_visits.category_visits, transaction 
 FROM transactions 
-LEFT JOIN product_visits ON transactions.fullvisitorid = product_visits.fullvisitorid 
-AND transactions.v2ProductName = product_visits.v2ProductName AND transactions.datetime_transaction = product_visits.datetime
-LEFT JOIN category_visits ON transactions.fullvisitorid = category_visits.fullvisitorid 
-AND transactions.v2ProductCategory = category_visits.v2ProductCategory AND transactions.datetime_transaction = category_visits.datetime
+LEFT JOIN product_visits 
+ON transactions.fullvisitorid = product_visits.fullvisitorid 
+AND transactions.v2ProductName = product_visits.v2ProductName 
+AND transactions.datetime_transaction = product_visits.datetime
+LEFT JOIN category_visits 
+ON transactions.fullvisitorid = category_visits.fullvisitorid 
+AND transactions.v2ProductCategory = category_visits.v2ProductCategory 
+AND transactions.datetime_transaction = category_visits.datetime
 
 ORDER BY transaction DESC, Datetime_transaction 
 
-#################################################################################################################################################################
+#########################################################################################################################
 
 SELECT DISTINCT fullvisitorid, device.deviceCategory, device.operatingSystem,trafficSource.campaign, 
 trafficSource.medium, geoNetwork.country, hp.v2ProductCategory, hp.v2ProductName, hp.productPrice /1000000 AS price,
